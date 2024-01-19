fbc05a391df0354b4760a5eff7f0adba52c953ae
Arthur-tu <artur-tulinov@mail.ru>
2024-01-20 01:40:21 +0300

    Написать endpoint с бизнес-логикой. Создать Swagger Open API и unit-тесты

--- src/main/java/ru/microservice/photoservice/utill/JsonHelper.java
+++ src/main/java/ru/microservice/photoservice/utill/JsonHelper.java
@@ -32,13 +32,4 @@ public class JsonHelper {
             return null;
         }
     }
-
-    public static <T> List<T> parseJsonArray(String json,
-                                             Class<T> classOnWhichArrayIsDefined)
-            throws IOException, ClassNotFoundException {
-        ObjectMapper mapper = new ObjectMapper();
-        Class<T[]> arrayClass = (Class<T[]>) Class.forName("[L" + classOnWhichArrayIsDefined.getName() + ";");
-        T[] objects = mapper.readValue(json, arrayClass);
-        return Arrays.asList(objects);
-    }
 }
